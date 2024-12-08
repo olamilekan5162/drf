52c6fdfc8cfa1af07b6557fea27389b844fde970
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-12-08 18:29:30 +0100

    worked on a post request in drf

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,22 +4,20 @@ import json
 from products.models import Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
 from products.serializers import ProductSerializer
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home (request, *args, **kwargs):
-   instance = Product.objects.all().order_by("?").first()
-   data = {}
-   if instance:
-    data = ProductSerializer(instance).data
-    return Response(data)
+   serializer = ProductSerializer(data=request.data)
+   if serializer.is_valid():
+     serializer.save()
+     return Response(serializer.data)
 
 @api_view(["GET"])
 def api_by_id(request, pk):
-  model_data = Product.objects.get(id=pk)
+  instance = Product.objects.get(id=pk)
   data = {}
-  if model_data:
-   data = model_to_dict(model_data)
-   return Response(data)
+  if instance:
+    data = ProductSerializer(instance).data
+    return Response(data)
