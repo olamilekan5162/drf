a04faf6e9972f835bfefeffe5f509d74e336db69
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-12-08 17:04:26 +0100

    learning how to do serialization of data itself

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 # from django.http import JsonResponse, HttpResponse
 import json
 from products.models import Product
-from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from products.serializers import ProductSerializer
 
 # Create your views here.
 @api_view(["GET"])
 def api_home (request, *args, **kwargs):
-   model_data = Product.objects.all().order_by("?").first()
+   instance = Product.objects.all().order_by("?").first()
    data = {}
-   if model_data:
-    data = model_to_dict(model_data)
+   if instance:
+    data = ProductSerializer(instance).data
     return Response(data)
 
 @api_view(["GET"])
